804c18241b91ddc69186d428108cf32278a536bc
Ketteiteki <kudryashov.kd@gmail.com>
2023-07-29 08:35:51 +0400

    add new animations

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
 import { animate, query, stagger, state, style, transition, trigger } from "@angular/animations";
+import { AboutMeLink } from 'src/app/models/AboutMeLink';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss'],
   animations: [
+    trigger('nicknameAnimation', [
+      transition(':enter', [
+        style({ opacity: 0}),
+        animate('.5s 100ms', style({opacity: 1}))
+      ])
+    ]),
+    trigger('avatarAnimation', [
+      transition(':enter', [
+        style({ opacity: 0}),
+        animate('.5s 400ms', style({opacity: 1}))
+      ])
+    ]),
+    trigger('bioAnimation', [
+      transition(':enter', [
+        style({ opacity: 0}),
+        animate('.5s 700ms', style({opacity: 1}))
+      ])
+    ]),
+    trigger('aboutMeLinkAnimation', [
+      transition(':enter', [
+        query(':enter', style({ transform: 'translateY(-15px)', opacity: 0 }), { optional: true }),
+        query(':enter', stagger('100ms', [
+          animate('200ms 1.2s', style({ transform: 'translateY(0)', opacity: 1 }))
+        ]), { optional: true })
+      ])
+    ])
   ]
 })
 export class AboutMeComponent {
-
+  links = [
+    new AboutMeLink("../../../assets/img/github.png", "https://github.com/kolosovpetro"),
+    new AboutMeLink("../../../assets/img/google-scholar.png", "https://scholar.google.com/citations?user=MpDEhEUAAAAJ&hl=en"),
+    new AboutMeLink("../../../assets/img/orcid.png", "https://orcid.org/0000-0002-6544-8880"),
+    new AboutMeLink("../../../assets/img/telegram.png", "https://t.me/razumovsky_r"),
+    new AboutMeLink("../../../assets/img/credly.png", "https://www.credly.com/users/petro-kolosov"),
+  ]
 }
