fd9b94627c52b43cfae2914ed0aef298c04b6de0
Ketteiteki <kudryashov.kd@gmail.com>
2023-08-17 16:16:36 +0400

    add config.json | display version

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import IConfig from 'src/app/models/interfaces/IConfig';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+  public config: IConfig | undefined;
 
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.httpClient
+      .get<IConfig>('../../../assets/config/config.json')
+      .subscribe({
+        next: (data) => {
+          this.config = data;
+        },
+      });
+  }
 }
